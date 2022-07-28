2e7767f089062b2dd504906f4b850370aa912a3b
izabelatalarek <izabelatalarek313131@gmail.com>
2022-07-28 10:20:41 +0200

    //item.h corrected

--- Item.h
+++ Item.h
@@ -1,30 +1,22 @@
 #pragma once
 #include<iostream>
 #include<list>
-#include <vector>
-
 class Item
 {
     int ID;
-    std::string name; 
+    std::string name;
     std::string description;
     std::string category;
     double price;
     bool availability;
     std::string supplier;
-    std::string category;
-    
     struct productCart {
         std::string name;
         std::string description;
         double price;
         bool availability;
         std::string supplier;
-        std::string category;
-       
     };
-public:
-    //std::vector<std::string> vectorCategory{ "fresh", "short expiry date", "at your own risk" };
     productCart prodCart;
 public:
     Item(int ID, std::string name, std::string description, double price, bool availability, std::string supplier, std::string category);
@@ -35,7 +27,4 @@ public:
     double getPrice();
     bool getAvailability();
     std::string getSupplier();
-    std::string getCategory();
-    int ranndomCategory();
-    void showProductCart();
 };
